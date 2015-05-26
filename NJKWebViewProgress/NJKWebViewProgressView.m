65b639060fc16faf041c24425152aa013a34cf1d
itay1336 <itay1336@gmail.com>
2015-05-25 17:55:53 -0700

    Update NJKWebViewProgressView.m
    support app extensions

--- NJKWebViewProgress/NJKWebViewProgressView.m
+++ NJKWebViewProgress/NJKWebViewProgressView.m
@@ -31,9 +31,11 @@
     _progressBarView = [[UIView alloc] initWithFrame:self.bounds];
     _progressBarView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
     UIColor *tintColor = [UIColor colorWithRed:22.f / 255.f green:126.f / 255.f blue:251.f / 255.f alpha:1.0]; // iOS7 Safari bar color
+#if defined(APP_EXTENSION)
     if ([UIApplication.sharedApplication.delegate.window respondsToSelector:@selector(setTintColor:)] && UIApplication.sharedApplication.delegate.window.tintColor) {
         tintColor = UIApplication.sharedApplication.delegate.window.tintColor;
     }
+#endif
     _progressBarView.backgroundColor = tintColor;
     [self addSubview:_progressBarView];
     
